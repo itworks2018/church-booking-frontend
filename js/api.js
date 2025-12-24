f8da878a40aab6eb61b2da52c671c42fc6e24bd8
itworks2018 <itworks.shimt2018@gmail.com>
2025-12-24 20:05:00 +0800

    updated frontend files

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://church-booking-backend.onrender.com/api'
+const API_BASE = 'https://church-booking-backend.onrender.com'
 
 let accessToken = localStorage.getItem('access_token') || null
 let userRole = localStorage.getItem('user_role') || null
